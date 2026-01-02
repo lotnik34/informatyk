f3465599b34ed661f373ee2c0404fd8fc1cdbad7
lotnik34 <lotnik9@o2.pl>
2026-01-02 18:57:52 +0100

    script.js

--- script.js
+++ script.js
@@ -9,19 +9,3 @@ function toggleTheme() {
     document.body.classList.toggle("dark");
     document.body.classList.toggle("light");
 }
-
-document.getElementById("contactForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
-
-    const status = document.getElementById("status");
-    const formData = new FormData(this);
-
-    const response = await fetch("http://127.0.0.1:5000/send", {
-        method: "POST",
-        body: formData
-    });
-
-    const result = await response.text();
-    status.textContent = result;
-    this.reset();
-});
