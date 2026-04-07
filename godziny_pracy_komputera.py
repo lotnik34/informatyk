478ce369c681fd66e2221c76b0e686110859873a
lotnik34 <lotnik9@o2.pl>
2026-04-07 22:05:56 +0200

    godziny_pracy_komputera.py

--- godziny_pracy_komputera.py
+++ godziny_pracy_komputera.py
@@ -91,7 +91,8 @@ stopka = ctk.CTkLabel(
     justify="left"
 )
 stopka.place(relx=0.02, rely=0.93, anchor="w")
-dodanie programu monitorowania czasu pracy komputera
+
 
 odswiez()
 app.mainloop()
+dodanie programu monitorowania czasu pracy komputera
